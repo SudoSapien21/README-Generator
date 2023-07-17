6f21e1883eb734b741dccfc1bbf39420a9240166
SudoSapien21 <sudosapien21@gmail.com>
2023-07-16 20:09:31 -0500

    installed the inquirer; added screenshots and and video to the README file.

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,19 +1,77 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function that returns a license badge based on the license value
+// If there is no license, it returns an empty string
+function renderLicenseBadge(license) {
+  if (license) {
+    return `![License](https://img.shields.io/badge/License-${license}-brightgreen)`;
+  }
+  return '';
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function that returns the license link based on the license value
+// If there is no license, it returns an empty string
+function renderLicenseLink(license) {
+  if (license) {
+    return `[License](https://opensource.org/licenses/${license})`;
+  }
+  return '';
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function that returns the license section of the README
+// If there is no license, it returns an empty string
+function renderLicenseSection(license) {
+  if (license) {
+    return `## License
 
-// TODO: Create a function to generate markdown for README
+This project is licensed under the ${license} license. Click [here](https://opensource.org/licenses/${license}) for more information.`;
+  }
+  return '';
+}
+
+// Function to generate markdown for the README
 function generateMarkdown(data) {
+  // Use the provided data to dynamically generate the README content
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+For any questions, please contact me:
+
+GitHub: [${data.github}](https://github.com/${data.github})
+
+Email: ${data.email}
 `;
 }
 
--- index.js
+++ index.js
@@ -1,13 +1,77 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the Title of your project?'
+    },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description for your project:'
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide a installation intruction for your project:'
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide a usage information of your project:'
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide a contribution guidelines for your project:'
+      },  
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide test intructions for your project:'
+      }, 
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'Apache 2.0', 'GNU GPLv3', 'None']
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Provide your GitHub Profile:'
+      },  
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Provide your email:'
+      },  
+  
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// TODO: Create a function to write the README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('README.md file has been generated!');
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    const markdown = generateMarkdown(data);
+    writeToFile('README.md', markdown);
+  });
+}
 
 // Function call to initialize app
 init();
